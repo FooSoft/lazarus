f766b12c2b320f94a88ba50524f160174e6c92d7
Alex Yatskov <alex@foosoft.net>
2019-01-02 20:37:00 -0800

    wip

--- platform/imgui/imgui.go
+++ platform/imgui/imgui.go
@@ -1,31 +1,57 @@
 package imgui
 
+// #include "native.h"
+import "C"
 import (
+	"unsafe"
+
 	"github.com/FooSoft/lazarus/graphics"
 	"github.com/FooSoft/lazarus/math"
 )
 
+func (*Context) DialogBegin(label string) bool {
+	labelC := C.CString(label)
+	defer C.free(unsafe.Pointer(labelC))
+	return bool(C.igBegin(labelC, nil, 0))
+}
+
+func (*Context) DialogEnd() {
+	C.igEnd()
+}
+
 func (*Context) Button(label string) bool {
-	// return imgui.Button(label)
-	return false
+	labelC := C.CString(label)
+	defer C.free(unsafe.Pointer(labelC))
+	return bool(C.igButton(labelC, C.ImVec2{}))
 }
 
 func (c *Context) Image(texture graphics.Texture) {
-	// c.ImageSized(texture, texture.Size())
+	c.ImageSized(texture, texture.Size())
 }
 
 func (*Context) ImageSized(texture graphics.Texture, size math.Vec2i) {
-	// imgui.Image(imgui.TextureID(texture.Id()), imgui.Vec2{X: float32(size.X), Y: float32(size.Y)})
+	C.igImage(
+		imTextureId(texture.Id()),
+		C.ImVec2{x: C.float(size.X), y: C.float(size.Y)},
+		C.ImVec2{0, 0},
+		C.ImVec2{1, 1},
+		C.ImVec4{1, 1, 1, 1},
+		C.ImVec4{0, 0, 0, 0},
+	)
 }
 
 func (*Context) SliderInt(label string, value *int, min, max int) bool {
-	// temp := int32(*value)
-	// result := imgui.SliderInt(label, &temp, int32(min), int32(max))
-	// *value = int(temp)
-	// return result
-	return false
+	labelC := C.CString(label)
+	defer C.free(unsafe.Pointer(labelC))
+	valueC := C.int(*value)
+	result := bool(C.igSliderInt(labelC, &valueC, (C.int)(min), (C.int)(max), nil))
+	*value = int(valueC)
+	return result
 }
 
 func (*Context) Text(label string) {
-	// imgui.Text(label)
+	labelStartC := C.CString(label)
+	labelEndC := (*C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(labelStartC)) + uintptr(len(label))))
+	defer C.free(unsafe.Pointer(labelStartC))
+	C.igTextUnformatted(labelStartC, labelEndC)
 }
